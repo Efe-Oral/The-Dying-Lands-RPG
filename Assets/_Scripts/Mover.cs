483f2cbd26f53e51f273eba1825a1fe469f29333
Efe Oral <efeoral@gmail.com>
2022-11-20 13:58:24 +0300

    animator blend added

--- Assets/_Scripts/Mover.cs
+++ Assets/_Scripts/Mover.cs
@@ -6,11 +6,13 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] Animator animator;
     NavMeshAgent agent;
 
     private void Start() 
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -19,6 +21,7 @@ public class Mover : MonoBehaviour
         {
            MoveToCursor();
         }
+        UpdateAnimator();
     }
 
         private void MoveToCursor()
@@ -30,5 +33,15 @@ public class Mover : MonoBehaviour
             {
                 agent.SetDestination(hit.point);
             }
+        }
+        private void UpdateAnimator()
+        {
+            // Get the speed form the navmesh agent
+            Vector3 velocity = agent.velocity; 
+            // Convert the agent's velocity to local velocity
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            // We only interested in the z velocity because that is tha axis our player walks in
+            float speed = localVelocity.z;
+            animator.SetFloat("forwardSpeed", speed);
         }
 }
