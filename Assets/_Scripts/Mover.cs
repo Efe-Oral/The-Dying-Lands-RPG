e698b8421c48ceb839ea6777dfe4fb3effd956bb
Efe Oral <efeoral@gmail.com>
2022-11-23 15:12:47 +0300

    Movement upgraded

--- Assets/_Scripts/Mover.cs
+++ Assets/_Scripts/Mover.cs
@@ -17,7 +17,7 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
         {
            MoveToCursor();
         }
