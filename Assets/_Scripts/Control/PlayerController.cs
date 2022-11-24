77bf8ab067fa0a9cd1a08dd143e25f6ea8e03170
Efe Oral <efeoral@gmail.com>
2022-11-24 15:04:48 +0300

    hover mouse added

--- Assets/_Scripts/Control/PlayerController.cs
+++ Assets/_Scripts/Control/PlayerController.cs
@@ -20,42 +20,40 @@ namespace RPG.Control
 
         private void Update()
         {
-            InteractWithCombat();
-            InteractWithMovement();
+            if(InteractWithCombat()){return;}
+            if(InteractWithMovement()){return;}
+            print("Nothing here");
         }
 
-        private void InteractWithMovement()
-        {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
-        }
-
-        private void InteractWithCombat()
-        {
-            if(Input.GetMouseButton(0))
-            {
-                RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-                foreach (RaycastHit hit in hits)
-                {
-                    CombatTarget target = hit.collider.GetComponent<CombatTarget>();
-                    if ( target!= null)
-                    {
-                        fighter.Attack(target);
-                    }
-                }
-            }
-        }
-
-        public void MoveToCursor()
+        private bool InteractWithMovement()
         {
             RaycastHit hit;
             bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
-                mover.MoveTo(hit.point);
+                if(Input.GetMouseButton(0))
+                {
+                    mover.MoveTo(hit.point);
+                }
+                return true;
             }
+            return false;
+        }
+
+        private bool InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.collider.GetComponent<CombatTarget>();
+                if (target == null) continue;
+                if(Input.GetMouseButtonDown(0))
+                {
+                    fighter.Attack(target);
+                }
+                return true;
+            }
+        return false;
         }
 
         private static Ray GetMouseRay()
