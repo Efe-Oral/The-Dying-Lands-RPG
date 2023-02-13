f1a3ac1ca5194babf9b384c03df246e3291baddf
Efe Oral <efeoral@gmail.com>
2023-02-13 21:10:04 +0300

    death anim. added

--- Assets/_Scripts/Combat/Health.cs
+++ Assets/_Scripts/Combat/Health.cs
@@ -6,16 +6,28 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
+        private bool isDead = false;
         [SerializeField] float health = 40f;
 
         public void TakeDamage(float damage)
         {
             health = health - damage;
-            if (health < 0) // health = Mathf.Max(health - damage, 0) Take the max between these valuse, so that it can not be minus.
+            if (health <= 0) // health = Mathf.Max(health - damage, 0) Take the max between these valuse, so that it can not be minus.
             {
+                if(isDead) return;
+                isDead = true;
                 health = 0;
+                Die();
             }
             Debug.Log(health);
+        }
+
+        public void Die()
+        {
+            if(isDead)
+            {
+                GetComponent<Animator>().SetTrigger("die");
+            }
         }
     }
 }
